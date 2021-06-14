0b7fd0c61f75c2497e59b9e89ecda091d92a748c
how melnyczuk <how@melnycz.uk>
2021-06-14 12:41:46 +0100

    Add cli

--- src/cli.py
+++ src/cli.py
@@ -2,21 +2,48 @@ import argparse
 
 from torch.utils.data import DataLoader
 
-from .repose import Repose
+from src.adapters.coco import Coco, CocoDataset
+from src.app import Repose
 
-parser = argparse.ArgumentParser(description="Run Repose GAN")
-parser.add_argument("data", type=str, help="Training data")
-parser.add_argument("--batch", help="Batch size")
-parser.add_argument("--workers", help="Number of workers")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Repose GAN")
+    parser.add_argument(
+        "data",
+        type=str,
+        help="Training data",
+    )
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        default=None,
+        help="Dir for saved samples",
+    )
+    parser.add_argument(
+        "--batch",
+        type=int,
+        default=100,
+        help="Batch size",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=1,
+        help="Number of epochs",
+    )
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=1,
+        help="Number of workers",
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-train_loader = DataLoader(
-    train_data,
-    batch_size=args.batch,
-    num_workers=args.workers,
-)
+    train_loader: DataLoader = DataLoader(
+        CocoDataset(args.data),
+        batch_size=args.batch,
+        num_workers=args.workers,
+    )
 
-repose = Repose()
-repose.train(train_loader, 1)
-repose.save_samples("train_samples.pkl")
+    repose = Repose(data_length=Coco.LENGTH)
+    repose.train(train_loader, args.epochs, save_path=args.outdir)
